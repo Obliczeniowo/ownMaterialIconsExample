875945ae564eedf5eab260f16d269f9688a86d97
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2019-11-16 07:18:49 +0100

    Add basic example of default material icon and add material module to import in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
